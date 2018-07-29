ffe8c44f96c14b4bfced106979d75352134146b2
Rustam Gilyaziev <rustam.gilyaziev@jetruby.com>
2018-07-29 15:09:49 +0300

    refactor(server): lint graphql-authentication lowdb adapter

--- server/entity/auth/adapter.ts
+++ server/entity/auth/adapter.ts
@@ -6,29 +6,29 @@ import { users } from '../../services/db'
 const findBy = (prop, value) => find(propEq(prop, value))
 
 export class GraphqlAuthenticationLowAdapter {
-  public findUserById(_, id) {
+  findUserById(_, id) {
     return users(findBy('id', id))
   }
-  public findUserByEmail(_, email) {
+  findUserByEmail(_, email) {
     const user = users(findBy('email', email))
 
     return user
   }
-  public async userExistsByEmail(_, email) {
+  async userExistsByEmail(_, email) {
     const user = this.findUserByEmail(_, email)
 
     return !!user
   }
 
   // the _createUser and _updateUser methods are just helper methods, they are not used by graphql-authentication.
-  public _createUser(_, data) {
+  _createUser(_, data) {
     const newUser = merge({ id: shortid.generate(), isSuper: false }, data)
     users.write(append(newUser))
 
     return newUser
   }
 
-  public _updateUser(_, userId, data) {
+  _updateUser(_, userId, data) {
     const findUser = findBy('id', userId)
 
     const user = users(findUser)
@@ -41,28 +41,28 @@ export class GraphqlAuthenticationLowAdapter {
     return updatedUser
   }
 
-  public createUserBySignup(ctx, data) {
+  createUserBySignup(ctx, data) {
     return this._createUser(ctx, data)
   }
-  public createUserByInvite(ctx, data) {
+  createUserByInvite(ctx, data) {
     return this._createUser(ctx, data)
   }
-  public updateUserConfirmToken(ctx, userId, data) {
+  updateUserConfirmToken(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
-  public updateUserLastLogin(ctx, userId, data) {
+  updateUserLastLogin(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
-  public updateUserPassword(ctx, userId, data) {
+  updateUserPassword(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
-  public updateUserResetToken(ctx, userId, data) {
+  updateUserResetToken(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
-  public updateUserInfo(ctx, userId, data) {
+  updateUserInfo(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
-  public updateUserCompleteInvite(ctx, userId, data) {
+  updateUserCompleteInvite(ctx, userId, data) {
     return this._updateUser(ctx, userId, data)
   }
 }
